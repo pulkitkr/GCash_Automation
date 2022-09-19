2982afbf5f455f588142033c394b181eb1f7666c
pulkitkr <96609093+pulkitkr@users.noreply.github.com>
2022-09-19 20:00:45 +0530

    Update GCASHScripts.java

--- src/test/java/com/GCash_GGivesScripts/GCASHScripts.java
+++ src/test/java/com/GCash_GGivesScripts/GCASHScripts.java
@@ -80,7 +80,7 @@ public class GCASHScripts {
 		GCASHBusiness.GsaveLogin(GSaveValidPhoneNumber , GSaveValidOTP);
 		ExtentReporter.jiraID = "PP-24";
 	}
-
+//Hello
 	@Test(priority = 9)
 	@Parameters({"amtPay"})
 	public void gSaveTransactionPage(String amtPay) throws Exception {
